6237c401532939380f32b53c33a00271e15f2dc0
ILYA <Desantt@mail.ru>
2023-02-23 14:45:48 +0300

    Add example

---
+++ Example1/Program.cs
@@ -0,0 +1,52 @@
+﻿// 1. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int InputInt(string msg)
+{
+    System.Console.Write($"{msg}>");
+    int value;
+
+    if (int.TryParse(Console.ReadLine(), out value))
+    {
+    return value;
+    }
+
+System.Console.WriteLine("Вы ввели не число");
+Environment.Exit(1);
+return 0;
+
+}
+
+bool ValidareCoords(int x, int y)
+{
+    if (x == 0 || y == 0)
+    {
+        System.Console.WriteLine("Одна из координат находится на оси");
+        return false;
+    }
+    return true;
+}
+
+int GetQuoter(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+    if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+    if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+    return 4;
+
+}
+
+int x = InputInt("Введите x");
+int y = InputInt("Введите y");
+if (ValidareCoords(x, y))
+{
+    System.Console.WriteLine($"Координаты {x}, {y} находятся в четверти {GetQuoter(x, y)} ");
+}
---
+++ Example2/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)
+// x >0, y<0
+
+int InputInt(string msg)                          //Название метода
+{
+    System.Console.Write($"{msg}>");                //вывод приглашения ко вводу
+    int value;                                      //Инициализация переменой
+
+    if (int.TryParse(Console.ReadLine(), out value))   // Проверка условия ввода + ввод со строки
+    {                                                   // если конвертация прошла правильно, то мы попадаем в ветку    
+    return value;                                       // которая находится в скобках после наименования функции. Если да
+    }                                                   // то выдает первое значение (ввод со строки). Нет второе(out)
+
+System.Console.WriteLine("Вы ввели не число");          
+Environment.Exit(1);                                    // если TryParse не сработало, выходим из программы (Exit) с кодом 1
+return 0;                                               // нужна для компиляции
+}
+
+bool ValidateQuoter(int quoter)
+{
+    if(quoter < 1 || quoter > 4)
+    {
+        System.Console.WriteLine("Заданы не правельные значения");
+        return false;
+    }
+    return true;
+}
+
+(int, int)GetCoords(int quoter)    //Картеж - набор переменых
+{
+    switch(quoter )                // Выбор четверти
+    {
+        case 1: return(1,1);        // что делаем при определении значения переменой  quoter. Вариант при quoter=1
+        break;                      // пишется для выхода из switch. Если был до этого return можно не писать
+        case 2: return(-1,1); 
+        break;
+        case 3: return(-1,-1); 
+        break;
+        default:                    //Вариация else (еслине выбрано любое другое значение)
+        return (1, -1);
+    }
+}
+
+int quoter = InputInt("Введите омер четверти");  // Вызываем функцию ввода с переменой quoter
+if(ValidateQuoter(quoter))                       // проверка правильности ввода (что это четверть)
+{
+    (int x, int y) = GetCoords(quoter);         // получение пары координат (x, y) в зависимости  от введеной четверти
+    System.Console.WriteLine($"Возможные координаты в четверти (quoter): {x}, {y} "); // вывод рузультата на экран
+}
